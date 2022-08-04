883d37fcbb124053d6383b5179e25dbbfce35c95
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-08-04 13:24:32 +0300

    second commit

--- app/src/main/java/com/example/dicerollerjuly31/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerjuly31/MainActivity.kt
@@ -1,25 +1,35 @@
-package com.example.dicerollerjuly31
+package com.example.dicerollerhw3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-
-    // function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = findViewById(R.id.btn_roll)
-        val numberText : TextView = findViewById(R.id.txt_number)
 
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
         }
+    }
+   
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
 
+        val diceImage : ImageView = findViewById(R.id.img_dice)
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
